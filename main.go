83509f15ceda40256312534c5419071c9c685a19
fancl <fancl@justcall.cn>
2023-05-18 17:17:56 +0800

    add health check supported

--- main.go
+++ main.go
@@ -17,39 +17,52 @@ var (
 	targetFlag = flag.String("target", "", "Target rewrite host")
 )
 
+func envVariable(name string, defaultVal string) string {
+	if val := os.Getenv(name); val != "" {
+		return val
+	}
+	return defaultVal
+}
+
 func main() {
 	var (
-		port    int
-		err     error
-		uri     *url.URL
-		target  string
-		address string
+		n         int
+		port      string
+		err       error
+		targetUri *url.URL
+		healthUri *url.URL
+		targetUrl string
+		healthUrl string
+		address   string
 	)
 	flag.Parse()
-	port = *portFlag
-	if port <= 0 {
-		port, _ = strconv.Atoi(os.Getenv("HTTP_PORT"))
-	}
-	if port <= 0 {
-		port = 80
-	}
-	target = *targetFlag
-	if target == "" {
-		target = os.Getenv("TARGET_URL")
-	}
-	if uri, err = url.Parse(target); err != nil {
+	port = envVariable("HTTP_PORT", strconv.Itoa(*portFlag))
+	targetUrl = envVariable("TARGET_URL", *targetFlag)
+	healthUrl = envVariable("HEALTH_URL", "")
+	if targetUri, err = url.Parse(targetUrl); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	log.Printf("reverse proxy url: %s\n", target)
+	if healthUrl != "" {
+		healthUri, err = url.Parse(healthUrl)
+	}
+	if n, err = strconv.Atoi(port); err != nil || n <= 0 {
+		port = "80"
+	}
+	log.Printf("reverse proxy url: %s\n", targetUrl)
 	reverseProxy := &httputil.ReverseProxy{}
 	reverseProxy.Rewrite = func(request *httputil.ProxyRequest) {
-		request.SetURL(uri)
-		if *debugFlag {
-			log.Printf("rewrite url %s -> %s", request.In.URL.String(), request.Out.URL.String())
+		if healthUri != nil && request.In.URL.Path == "/_/health" {
+			request.SetURL(healthUri)
+			request.Out.URL.Path = "/"
+		} else {
+			request.SetURL(targetUri)
+			if *debugFlag {
+				log.Printf("rewrite url %s -> %s", request.In.URL.String(), request.Out.URL.String())
+			}
 		}
 	}
-	address = fmt.Sprintf(":%d", port)
+	address = fmt.Sprintf(":%s", port)
 	log.Printf("http listen on %s\n", address)
 	if err = http.ListenAndServe(address, reverseProxy); err != nil {
 		fmt.Println(err)
